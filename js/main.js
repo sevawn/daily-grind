77a22b5a4c83bc1892f7956a0cf0059491bd4861
Bill Newman <william.newman@seattlecolleges.edu>
2026-05-05 12:35:52 -0700

    Updates main.js
    Fixes selector

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Fixes HTML/CSS Checker links
 document.addEventListener("DOMContentLoaded", function() {
     var currentUrl = window.location.href;
-    document.getElementById("html-validate").href = "https://validator.w3.org/nu/?doc=" + encodeURIComponent(currentUrl);
-    document.getElementById("css-validate").href = "https://jigsaw.w3.org/css-validator/validator?uri=" + encodeURIComponent(currentUrl);
+    document.getElementById("html-checker").href = "https://validator.w3.org/nu/?doc=" + encodeURIComponent(currentUrl);
+    document.getElementById("css-checker").href = "https://jigsaw.w3.org/css-validator/validator?uri=" + encodeURIComponent(currentUrl);
 });
